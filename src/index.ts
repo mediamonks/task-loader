2c4f2e95a9995c468eea02f38cd0707ffc39a4b7
Lars van Braam <lars@mediamonks.com>
2018-03-26 20:14:10 +0200

    Export the LoadHowlerAudioTask Update the travis configuration

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { default as LoadImageTask } from './lib/task/LoadImageTask';
 export { default as LoadJsonTask } from './lib/task/LoadJsonTask';
 export { default as LoadScriptTask } from './lib/task/LoadScriptTask';
 export { default as LoadVideoTask } from './lib/task/LoadVideoTask';
+export { default as LoadHowlerAudioTask } from './lib/task/LoadHowlerAudioTask';
 
 export { default as TaskLoaderEvent } from './lib/event/TaskLoaderEvent';
 export { default as cacheManager } from './lib/CacheManager';
